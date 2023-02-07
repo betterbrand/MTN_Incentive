8fe3d255942840848e58fcb6ad31c20c1f2b8613
Kokodavid <kingkgdave@gmail.com>
2023-02-07 16:29:21 +0300

    tweaking contract

--- contracts/NetworkNationSplit.sol
+++ contracts/NetworkNationSplit.sol
@@ -1,33 +1,74 @@
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
-contract NetworkNationSplit {
-    address[] partners;
-    uint[] percentages;
-    uint totalPercentage;
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-    constructor(address[] memory _partners, uint[] memory _percentages) public {
-        require(
-            _partners.length == _percentages.length,
-            "The number of partners and percentage must match."
-        );
+contract NetworkNationSplit is UUPSUpgradeable, OwnableUpgradeable {
+    // Store an array of addresses and their corresponding percentages
+    mapping(address => uint256) public feeDistribution;
+    mapping(uint256 => uint256) public totalPercentage;
 
-        for (uint i = 0; i < _partners.length; i++) {
-            partners.push(_partners[i]);
-            percentages.push(_percentages[i]);
-            totalPercentage += _percentages[i];
-        }
+    // Store the network admin address
+    address public networkAdmin;
 
-        require(
-            totalPercentage == 100,
-            "The total percentage must be 100."
-        );
+    modifier only_admin {
+        require(msg.sender == networkAdmin, "");
+        _;
     }
 
-    function distributeFee(uint fee) public {
-        uint share;
-        for (uint i = 0; i < partners.length; i++) {
-            share = (fee * percentages[i]) / 100;
-            partners[i].transfer(share);
-        }
+    function initialize(address _admin) public initializer {
+        __Context_init_unchained();
+        __Ownable_init_unchained();
+        __NetworkNationSplit_init_unchained(_admin);
+        __UUPSUpgradeable_init_unchained();
     }
-}
+
+    function __NetworkNationSplit_init_unchained(address _admin) private onlyInitializing {
+        networkAdmin = _admin;
+    }
+
+    // Function to set the network admin address
+    function setNetworkAdmin(address _networkAdmin) public only_admin {
+        networkAdmin = _networkAdmin;
+    }
+
+    // Function to set the fee distribution for an address
+    function setFeeDistribution(address _address, uint _percentage) public only_admin {
+       
+        feeDistribution[_address] = _percentage;
+    }
+
+
+  function collectFee(uint amount, address[]memory partners) public payable {
+        
+        // Calculate the total percentage
+
+            require (msg.value == amount, "Insufficient amount sent");
+            
+            //to do : Implement Split among addresses
+            uint256 j = 0;
+
+            for(j; j < partners.length; j++) {
+                require(checkAddressValue(partners[j]), "Address is not a partner");
+
+                uint256 _percentage = feeDistribution[partners[j]];
+
+                payable(partners[j]).transfer(msg.value * _percentage);
+
+            }
+
+
+        }
+
+
+    function checkAddressValue(address _address) public view returns (bool) {
+    return feeDistribution[_address] != 0;
+   }
+
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {
+    }
+
+    }
+
